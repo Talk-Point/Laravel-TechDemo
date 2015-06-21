aeae59fb9d7eb058bcc95eb81854b275c8ddcde1
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2015-06-21 16:33:31 +0200

    install pda/pheanstalk package

---
+++ Queue/app/Commands/SendEmail.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Commands;
+
+use App\Commands\Command;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Queue;
+
+class SendEmail extends Command implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct($string)
+    {
+        
+    }
+}
